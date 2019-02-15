d735abfe542833efe24b9449f5030410b7d0682e
Jérémy Lejeune <jeremy.lejeune@zenika.com>
2019-02-15 18:25:12 +0100

    Format code

--- src/Puzzle.elm
+++ src/Puzzle.elm
@@ -1,119 +1,150 @@
 import Browser
-
-import Html exposing (Html, button, div, h1, text)
-import Html.Events exposing (onClick)
-
 import Collage exposing (..)
 import Collage.Layout exposing (stack)
 import Collage.Render exposing (svg)
 import Color exposing (Color)
+import Html exposing (Html, button, div, h1, text)
+import Html.Events exposing (onClick)
+
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- Model
 
-type alias Model = {}
 
-init: Tile
+type alias Model =
+    {}
+
+
+init : Tile
+
+
+
 -- init = [Side (Connected False) Up, Side Empty Right, Side (Connected True) Down, Side Empty Left]
-init = {
-    sides = [Connected False, Empty, Connected True, Empty],
-    orientations = [Up, Right, Down, Left]
+
+
+init =
+    { sides = [ Connected False, Empty, Connected True, Empty ]
+    , orientations = [ Up, Right, Down, Left ]
     }
 
-type Orientation = Up | Left | Down | Right
+
+type Orientation
+    = Up
+    | Left
+    | Down
+    | Right
+
 
 type Side
     = Connected Bool
     | Empty
 
+
+
 -- type alias Side = {connection: Connection, orientation: Orientation}
 
-type alias Tile = {
-    sides: List Side,
-    orientations: List Orientation
+
+type alias Tile =
+    { sides : List Side
+    , orientations : List Orientation
     }
 
+
 hasConnection : Side -> Bool
-hasConnection side = 
+hasConnection side =
     case side of
         Connected _ ->
             True
-    
+
         Empty ->
             False
+
 
 isConnected : Side -> Bool
 isConnected side =
     case side of
         Connected True ->
-            True 
-        
-        Connected False -> 
+            True
+
+        Connected False ->
             False
 
         Empty ->
             False
 
+
 rotateLeft : List a -> List a
-rotateLeft list = 
+rotateLeft list =
     case list of
         [] ->
             []
-        
+
         x :: xs ->
-            xs ++ [x]
+            xs ++ [ x ]
+
+
 
 -- Update
 
-type Msg = Rotate
+
+type Msg
+    = Rotate
+
+
 update : Msg -> Tile -> Tile
 update msg tile =
     case msg of
         Rotate ->
             tile
 
+
+
 -- View
 
-testTile tile =  stack
-            [ line 50
-                |> traced (defaultLineStyle),
-            square 50
-                |> filled (uniform Color.lightYellow)
-            ]
-            |> svg
 
-drawSide: Side -> Orientation -> Path
+testTile tile =
+    stack
+        [ line 50
+            |> traced defaultLineStyle
+        , square 50
+            |> filled (uniform Color.lightYellow)
+        ]
+        |> svg
+
+
+drawSide : Side -> Orientation -> Maybe Path
 drawSide side orientation =
     case hasConnection side of
         True ->
-            orientationToPath orientation
-    
-        False ->
-            segment (0,0) (0,0)
+            Just (orientationToPath orientation)
 
--- drawTile: Tile -> Collage Msg
--- drawTile tile = List.map2 drawSide tile.sides tile.orientations
-                     
+        False ->
+            Nothing
+
 
 orientationToPath : Orientation -> Path
 orientationToPath orientation =
     case orientation of
         Up ->
-          segment (0,0) (0,25)  
-    
+            segment ( 0, 0 ) ( 0, 25 )
+
         Right ->
-            segment (0,0) (25,0)
+            segment ( 0, 0 ) ( 25, 0 )
 
         Down ->
-            segment (0,0) (0,-25)
-        
+            segment ( 0, 0 ) ( 0, -25 )
+
         Left ->
-            segment (0,0) (-25,0)
+            segment ( 0, 0 ) ( -25, 0 )
+
 
 view : Tile -> Html Msg
 view tile =
-    div [] 
+    div []
         [ button [ onClick Rotate ] [ text "Rotate" ]
-        , div [] [ testTile init ] ]
+        , div [] [ testTile init ]
+        ]
